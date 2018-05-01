7623b791bdbf4fa3f84ab85c9d8e702afb2e0b78
Bear Shuford <bearshuford@me.com>
2018-04-30 23:37:14 -0400

    project setup and view WIP

---
+++ FrontEndTest/Models/ContactFormModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace FrontEndTest.Models
+{
+    public class ContactFormModel
+    {
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+        public string Telephone { get; set; }
+    }
+}
